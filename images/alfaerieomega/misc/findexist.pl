82985d5de4709fb25b2d25e4b126916168a67ef9
jeff <jeff@jeffs-MacBook-Pro.local>
2009-06-12 22:36:31 -0700

    Check if identical

--- images/alfaerieomega/misc/findexist.pl
+++ images/alfaerieomega/misc/findexist.pl
@@ -1,3 +1,5 @@
+use File::Slurp qw/slurp/;
+
 open(FH,"<misc.txt")||die;
 print "<table><tr><th>misc</th><th>misc image</th><th>existing</th></tr>\n";
 while(<FH>)
@@ -5,11 +7,31 @@ while(<FH>)
     chomp;
     my $path = $_;
     $path =~ s(^\.\/)();
-    my ($dir, $fn) = split m(/), $path;
-    if (-e("../$fn")) {
+    my @a = split m(/), $path;
+    my $fn = pop @a;
+    my $dir = join("/", @a);
+
+    my $existing = "../$fn";
+
+    my $info = "";
+    if (same($existing, $path)) {
+        $info = "identical";
+    }
+
+    if (-e($existing)) {
         print <<HTML;
-<tr><td>$dir/$fn</td><td><img src="$dir/$fn"></td><td><img src="../$fn"></td></tr>
+<tr><td>$dir/$fn</td><td><img src="$dir/$fn"></td><td><img src="../$fn"></td><td>$info</td></tr>
 HTML
     }
 }
 print "</table>\n";
+
+# Return whether two files are identical
+# Not used, since none were exactly byte-wise equal
+sub same
+{
+    my ($p1, $p2) = @_;
+
+    return -s($p1) == -s($p2) && slurp($p1) eq slurp($p2);
+}
+
