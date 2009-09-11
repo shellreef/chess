f5fe80cc02250dfcb8328e6932dcac7af5c94de7
jeff <jeff@jeffs-MacBook-Pro.local>
2009-09-10 20:41:28 -0700

    Use JSON for game index

--- games/pindex.pl
+++ games/pindex.pl
@@ -12,13 +12,20 @@ use File::Find;
 # install with: sudo cpan Chess::PGN::Parse
 use Chess::PGN::Parse;
 
+# sudo cpan JSON
+use JSON;
+
+our @result;
+
 find(\&process_file, ".");
+
+print to_json(\@result), "\n";
 
 sub process_file
 {
     my ($filename) = $_;
     my ($full_filename) = $File::Find::name;
-    my ($pgn);
+    my ($pgn, $count, @games);
 
     return if $filename !~ m/[.]pgn$/;
 
@@ -29,8 +36,23 @@ sub process_file
         my $offset = 0;
         my @moves = $pgn->moves();
         # TODO: get offset somehow??
-        my @a = ($full_filename, $offset, $pgn->white(), $pgn->black(), $pgn->event(), $pgn->result(), "", scalar @moves);
-        print join("\t", @a), "\n";
+        my %h = (
+            filename => $full_filename, 
+            offset => $offset, 
+            white => $pgn->white(), 
+            black => $pgn->black(), 
+            event => $pgn->event(), 
+            result => $pgn->result(), 
+            opening => "",   # TODO
+            size => scalar @moves);
+    
+        $count += 1;
+        push @games, \%h;
+    }
+
+    if (@games == 1) {
+        # TODO: print all games. we only print one now, since load_pgn parses all.
+        push @result, $games[0];
     }
 }
 
